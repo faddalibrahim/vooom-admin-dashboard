ed02a441af5f9f68dd2015717dff5e52b1bae910
Faddal Ibrahim <faddalibrahim@gmail.com>
2022-04-26 16:22:36 +0000

    lets gooooo

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # VOOOM ADMIN DASHBOARD
+
+1. clone repo
+2. do `npm install` to get all the libraries
+3. create a new branch, call it anything
+4. start working and push to your branch!!!! dont mess up 😂😂😂
+5. when you are done, make pull request with **dev** branch
--- src/pages/playground/Playground.js
+++ src/pages/playground/Playground.js
@@ -1,5 +1,6 @@
 //libraries
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 // styles
 import playgroundStyles from "./playground.module.scss";
@@ -10,13 +11,15 @@ import { decrement, increment } from "redux/slices/counterSlice";
 
 // utils
 
+const pages = ["home", "transactions", "drivers", "buses", "sidebar"];
+
 const Playground = () => {
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
 
   return (
     <div className={playgroundStyles.playground}>
-      <p>Ready to Redux</p>
+      <p>Gentleman, make sure you dont mess up the repo 😂😂</p>
       <div>
         <button
           aria-label="Decrement value"
@@ -31,6 +34,14 @@ const Playground = () => {
         >
           +
         </button>
+      </div>
+      <br />
+      <div>
+        {pages.map((page) => (
+          <small key={page}>
+            <NavLink to={`/${page}`}>{page}</NavLink>
+          </small>
+        ))}
       </div>
     </div>
   );
