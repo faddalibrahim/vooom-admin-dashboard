3ee028a023ead753fefe9cc8af7db34bc759455b
Faddal Ibrahim <faddalibrahim@gmail.com>
2022-04-29 07:55:23 +0000

    mock data works

--- src/pages/transactions/mockData.js
+++ src/pages/transactions/mockData.js
@@ -1,12 +1,58 @@
 export const ticketsData = [
-  {
-    id: 1,
-    ticketType: "threeCedi",
-    quantity: 45,
-  },
-  {
-    id: 2,
-    ticketType: "oneCedi",
-    quantity: 100,
-  },
+  [
+    {
+      id: 1,
+      ticketType: "threeCedi",
+      quantity: 45,
+      amount: 135,
+    },
+    {
+      id: 2,
+      ticketType: "oneCedi",
+      quantity: 100,
+      amount: 100,
+    },
+  ],
+  [
+    {
+      id: 3,
+      ticketType: "threeCedi",
+      quantity: 60,
+      amount: 180,
+    },
+    {
+      id: 4,
+      ticketType: "oneCedi",
+      quantity: 90,
+      amount: 90,
+    },
+  ],
+  [
+    {
+      id: 5,
+      ticketType: "threeCedi",
+      quantity: 10,
+      amount: 30,
+    },
+    {
+      id: 6,
+      ticketType: "oneCedi",
+      quantity: 80,
+      amount: 80,
+    },
+  ],
+  [
+    {
+      id: 5,
+      ticketType: "threeCedi",
+      quantity: 100,
+      amount: 300,
+    },
+    {
+      id: 6,
+      ticketType: "oneCedi",
+      quantity: 50,
+      amount: 50,
+    },
+  ],
 ];
